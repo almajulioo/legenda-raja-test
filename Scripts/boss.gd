8f7f7d01ffa830ee6a63433447a7c7f6288f9559
almajulioo <alma.122140057@student.itera.ac.id>
2024-11-17 03:29:32 +0700

    Add Boss

---
+++ Scripts/boss.gd
@@ -0,0 +1,79 @@
+extends CharacterBody2D
+
+class_name Boss
+@onready var animated_sprite_2d: AnimationController = $AnimatedSprite2D
+@onready var left_collision: CollisionShape2D = $CombatSystem/WeaponHitbox/LeftCollision
+@onready var right_collision: CollisionShape2D = $CombatSystem/WeaponHitbox/RightCollision
+@onready var body_hitbox_collision: CollisionShape2D = $CombatSystem/BodyHitbox/CollisionShape2D
+
+
+var SPEED = 10000
+var direction : Vector2
+var dashing = false
+var idle = true
+var chase = false
+var canAttack = true
+var attacking = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float):
+	if has_node("../Player"):
+		var boss = get_node("../Boss")
+		var player = get_node("../Player")
+		
+		if attacking and canAttack: 
+			direction = (player.position - boss.position).normalized()
+		elif not attacking and chase:
+			direction = (player.position - boss.position).normalized()
+		else: 
+			direction = Vector2.ZERO
+		
+		if direction: 
+			velocity = direction * SPEED * delta
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+			velocity.y = move_toward(velocity.y, 0, SPEED * delta)
+			
+		
+	if attacking and canAttack:
+		$TimerCanAttack.start(1) 
+		canAttack = false
+		animated_sprite_2d.play_attack_animation(velocity)
+	elif chase:
+		animated_sprite_2d.play_movement_animation(velocity)
+	else:
+		animated_sprite_2d.play_idle_animation()
+		
+	move_and_slide()
+
+
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = true
+		idle = false
+	
+
+func _on_player_detector_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = false
+		idle = true
+		
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		attacking = true
+		
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		attacking = false
+
+
+func _on_timer_can_attack_timeout() -> void:
+	canAttack = true
